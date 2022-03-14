dccacf9f108d5479e8cc48d3c5704a3ba2bd343e
Priyanka7011 <pk3201134@gmail.com>
2022-03-14 13:51:45 +0530

    post page

--- project/mainApp/forms.py
+++ project/mainApp/forms.py
@@ -11,6 +11,8 @@ class BlogForm(forms.ModelForm):
     
     def __init__(self,*args,**kwargs):
         super(BlogForm,self).__init__(*args,**kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'customTextField'
         
 
         
--- project/project/settings.py
+++ project/project/settings.py
@@ -128,3 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
